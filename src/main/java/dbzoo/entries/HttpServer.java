d481b2d8c069a0bac8e56eea6ab742f2a472d5f6
Christian Gram Kalhauge <christian@kalhauge.dk>
2020-10-02 10:52:55 +0200

    Now with server

--- src/main/java/dbzoo/entries/HttpServer.java
+++ src/main/java/dbzoo/entries/HttpServer.java
@@ -1,18 +1,47 @@
 package dbzoo.entries;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class HttpServer {
 
     public static void main(String[] args) {
-        try(ServerSocket severSocket = new ServerSocket(8080)) {
-            System.out.println("[SERVER] Started server at: " + severSocket);
+        try(ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("[SERVER] Started server at: " + serverSocket);
 
-            Socket socket = severSocket.accept();
+            Socket socket = serverSocket.accept();
             System.out.println("[SERVER] Connected to: " + socket);
 
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            String line;
+            while(!(line = reader.readLine()).isEmpty()) {
+                System.out.println(line);
+            }
+
+            StringWriter sw = new StringWriter();
+            PrintWriter w = new PrintWriter(sw);
+            w.println("<html>");
+            w.println("<body>");
+            w.println("<h1>Hello, World!</h1>");
+            w.println("</body>");
+            w.println("</html>");
+
+            String msg = sw.toString();
+            byte [] content = msg.getBytes(StandardCharsets.UTF_8);
+
+            String response = "HTTP/1.1 200 OK\r\n"
+                            + "Content-Type: text/html; charset=utf-8\r\n"
+                            + "Content-Length: " + content.length + "\r\n"
+                            + "\r\n";
+
+            var out = socket.getOutputStream();
+            out.write(response.getBytes(StandardCharsets.UTF_8));
+            out.write(content);
+            out.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
